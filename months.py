973eeefc0815516f80ef4e583cf81cd94d7e8c1d
shyadav708060-ai <https://github.com/shyadav708060-ai/pythoncode.git>
2026-09-04 17:24:43 -0400

    months-name

---
+++ months.py
@@ -0,0 +1,13 @@
+list = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+print(list[0])
+print(list[1])
+print(list[2])
+print(list[3])
+print(list[4])
+print(list[5])
+print(list[6])
+print(list[7])
+print(list[8])
+print(list[9])
+print(list[10])
+print(list[11])     
